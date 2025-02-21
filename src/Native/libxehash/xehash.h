e76594b60c33732ba28533df0503e712c9905780
ysmreg <33682244+ysmreg@users.noreply.github.com>
2025-02-21 16:18:35 +0900

    fix build

--- src/Native/libxehash/xehash.h
+++ src/Native/libxehash/xehash.h
@@ -5,13 +5,18 @@
 
 #ifdef _WIN32
     #define XE_EXPORT __declspec(dllexport)
+    #define XE_CALL __cdecl
 #else
     #define XE_EXPORT __attribute__((visibility("default")))
+    #define XE_CALL
 #endif
 
+#ifdef __cplusplus
 extern "C" {
+#endif
 
+XE_EXPORT void XE_CALL xehash(const uint8_t* input, uint8_t* output);
 
-XE_EXPORT void __cdecl xehash(const uint8_t* input, uint8_t* output);
-
+#ifdef __cplusplus
 }
+#endif
